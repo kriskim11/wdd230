de001890df7c2fc35411c171f69b692b21726578
kriskim11 <131200162+kriskim11@users.noreply.github.com>
2024-01-13 18:31:08 -0800

    added image and js
    added image

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,11 @@ const month = currentDate.getMonth()+1;
 const day = currentDate.getDate();
 document.getElementById("dateDisplay").textContent = `©${month}-${day}-${year}`
 
+const editableContent = document.getElementById("editableContent");
+const lastEditedSpan = doscument.getElementById("lastEdited");
+
+editableContent.addEventListener("input",function(){
+    const currentDate =new Date();
+    const formattedDate = currentDate.toLocaleDateString();
+    lastEditedSpan.textContent = formattedDate;
+});
